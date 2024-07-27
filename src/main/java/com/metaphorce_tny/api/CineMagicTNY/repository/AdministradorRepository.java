5ee2ad2f122fa1a8884720cd045c66f704e81710
Tanya Rojas <tanyaimt@gmail.com>
2024-07-26 23:55:00 -0600

    Definir repositorio Administrador and comment unused line

--- src/main/java/com/metaphorce_tny/api/CineMagicTNY/repository/AdministradorRepository.java
+++ src/main/java/com/metaphorce_tny/api/CineMagicTNY/repository/AdministradorRepository.java
@@ -1,13 +1,13 @@
 package com.metaphorce_tny.api.CineMagicTNY.repository;
 
-import com.metaphorce_tny.api.CineMagicTNY.model.Usuario;
+import com.metaphorce_tny.api.CineMagicTNY.model.Administrador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /*import java.util.List;*/
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+public interface AdministradorRepository extends JpaRepository<Administrador,Long> {
 
 
 }
--- src/main/java/com/metaphorce_tny/api/CineMagicTNY/repository/EspectadorRepository.java
+++ src/main/java/com/metaphorce_tny/api/CineMagicTNY/repository/EspectadorRepository.java
@@ -4,7 +4,7 @@ import com.metaphorce_tny.api.CineMagicTNY.model.Espectador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+/*import java.util.List;*/
 
 @Repository
 public interface EspectadorRepository extends JpaRepository<Espectador,Long> {
