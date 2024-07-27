f82c49e2b105f4aa1de6f8b74e827f1ecdc175aa
tanyaimt@gmail.com <tanya.rojas@ibm.com>
2024-07-26 20:24:13 -0600

    Download & extract springboot

---
+++ src/test/java/com/metaphorce_tny/CineMagicTNY/CineMagicTnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaphorce_tny.CineMagicTNY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineMagicTnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
